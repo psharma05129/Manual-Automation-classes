0cca74b230351b6a82939996ff44269107c880d5
psharma05129 <ps03069004@gmail.com>
2025-06-06 17:11:56 +0530

    Add files via upload

---
+++ NumberCalculation.java
@@ -0,0 +1,30 @@
+package MyFirstPackage;
+
+public class NumberCalculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Math = 55;
+		int Science = 75;
+		int English = 80;
+		int divide = 3;
+
+		double  average = (Math+Science+English)/ (double) divide;
+
+		System.out.println(" Math Number is : " + Math);
+		System.out.println(" Science Number is : " + Science);
+		System.out.println(" English Number is : " + English);
+		
+		System.out.println("Total Marks is : " + (Math+Science+English));
+		
+		System.out.println(" Average is : " + average);
+		
+			
+
+
+
+			
+
+	}
+
+}
