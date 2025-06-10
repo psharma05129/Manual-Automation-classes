35f13f528529c6b0e4d2846a855dafcb0e16fc62
psharma05129 <ps03069004@gmail.com>
2025-06-10 18:15:19 +0530

    Add files via upload

---
+++ PrintNumber.java
@@ -0,0 +1,17 @@
+package LoopJava;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 100; i++) {
+            if (i % 5 == 0) {
+                continue; // Skip multiples of 5
+            }
+            System.out.println(i);
+        }
+    }
+
+	}
+
+
